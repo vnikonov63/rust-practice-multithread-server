d46f3950eacf3239a28ef4563056baae4caec305
Vasilii Nikonov <nikonvasilii@gmail.com>
2026-04-23 19:45:34 -0700

    EDIT better zsh script so the user can see the results for different modes together in the same fike

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+echo "" > benchmark.txt
+
 for MODE in spawn-infinite-threads single-thread-tokio multi-thread-tokio
 do
   echo "RUNNING $MODE"
@@ -8,7 +10,8 @@ do
   # Make sure the server actually loads
   sleep 2
 
-  wrk -t4 -c100 -d30s -s scripts/root-60-sleep-20-404-20.lua http://127.0.0.1:7878 > benchmarks/$MODE.txt
+  echo "---\n$MODE:\n---" >> benchmark.txt
+  wrk -t4 -c100 -d30s -s scripts/root-60-sleep-20-404-20.lua http://127.0.0.1:7878 >> benchmark.txt
 
   kill $PID
 done
